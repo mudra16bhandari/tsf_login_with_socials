437bcf1e8e812b8138fe6b9ff7b7b87370add554
Mudra Bhandari <mudrabhandari123@yahoo.in>
2021-01-15 20:12:46 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/mudrab/login_with_socials/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mudrab.login_with_socials
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:login_with_socials/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login with Socials',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Login(),
+    );
+  }
+}
